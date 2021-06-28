9a38dc90aa2902ee328789aca1f5eca3b87f3f24
Claude Fried <claude.fried@flatironschool.com>
2021-06-28 11:35:26 -0400

    adding experimental scoring system

--- app.py
+++ app.py
@@ -85,12 +85,13 @@ SIDEBAR = html.Div([
         html.Hr(),
         dbc.Row(
             dbc.Nav([
-            dbc.NavLink("F1antasy League", href="/", active="exact"),
-            dbc.DropdownMenu(
-                [dbc.NavLink(team, href=f'/{"-".join(team.split())}', active="exact")
-                 for team in TEAM_NAMES_LST],
-                label='Teams'
-                ),
+                dbc.NavLink("F1antasy League", href="/", active="exact"),
+                dbc.DropdownMenu(
+                    [dbc.NavLink(team, href=f'/{"-".join(team.split())}', active="exact")
+                    for team in TEAM_NAMES_LST],
+                    label='Teams'
+                    ),
+                dbc.NavLink('EXPERIMENTAL', href='experimental', active='exact')
                 ],
             vertical=True,
             # pills=True,
@@ -180,6 +181,93 @@ LEAGUE = html.Div(id='league', children=[
     dbc.Row(
         dbc.Col(
             dcc.Graph(id='league_bar', figure=LEAGUE_BAR),
+            width=CHART_WIDTH
+        ),
+        justify='center'
+    ),
+    ]
+)
+###
+# Experimental
+###
+EXP_leaderboard = (
+    f1.results[f1.results['RaceID'] > 202100]
+    .merge(fantasy_rosters, on='DriverID')
+    .drop_duplicates()
+    .groupby('Team').sum()[['Position']]
+    .sort_values('Position', ascending=True)
+    .reset_index()
+)
+EXP_pts_per_round = (
+    f1.results.loc[f1.results['RaceID'] > 202100]
+    .merge(fantasy_rosters[['Team', 'DriverID']], 
+           on='DriverID')
+    .merge(f1.races[['RaceID', 'Round']])
+    .sort_values(['RaceID', 'Position'])
+    .loc[:, ['Position', 'Team', 'Round']]
+    .groupby(['Round', 'Team']).sum()
+    .reset_index()
+)
+EXP_cumulative_points = (
+    EXP_pts_per_round
+    .groupby(['Team', 'Round']).sum()
+    .groupby('Team').cumsum()
+    .reset_index()
+)
+EXP_cumulative_points['Position'] = -EXP_cumulative_points['Position']
+for round_num in EXP_cumulative_points['Round'].unique():
+    rnd_df = EXP_cumulative_points[EXP_cumulative_points['Round'] == round_num]['Position'].values
+    n = rnd_df.max()
+    EXP_cumulative_points.loc[EXP_cumulative_points['Round'] == round_num, 'PositionDifference'] = (rnd_df - n)
+
+EXP_LEADERBOARD = html.Div(id='exp-leaderboard', children=[
+    dbc.Row(
+        dbc.Col([
+            dbc.Row(html.H3('Experimental Leaderboard'), justify='center'),
+            dbc.Row(
+                html.P('The points system in this experimental section is golf-like where the lowest score is best. Points are awarded by finishing position (1-20) for each driver (1 being best, 20 being worst).'), 
+                justify='center'),
+            dbc.Table.from_dataframe(
+                EXP_leaderboard, 
+                striped=True, 
+                bordered=True, 
+                hover=True,
+                size='sm'),
+            ],
+            width=4
+            ),
+        justify='center',
+    ),
+])
+
+EXP_LEAGUE_LINE = (
+    px.line(
+        x='Round', y='PositionDifference', color='Team',
+        data_frame=EXP_cumulative_points, 
+        title='Experimental - Total Position Difference - 2021 Season',
+        )
+    .update_traces(mode='lines+markers')
+    .update_layout(paper_bgcolor='rgba(0,0,0,0)', autosize=True)
+)
+EXP_LEAGUE_BAR = (
+    px.bar(
+        x='Round', y='Position', color='Team', 
+        data_frame=EXP_pts_per_round,
+        title='Points by Round',
+        )
+    .update_layout(paper_bgcolor='rgba(0,0,0,0)', autosize=True)
+)
+EXP_LEAGUE = html.Div(id='exp-league', children=[
+    dbc.Row(
+        dbc.Col(
+            dcc.Graph(id='exp_league_line', figure=EXP_LEAGUE_LINE),
+            width=CHART_WIDTH
+            ),
+        justify='center'
+    ),
+    dbc.Row(
+        dbc.Col(
+            dcc.Graph(id='exp_league_bar', figure=EXP_LEAGUE_BAR),
             width=CHART_WIDTH
         ),
         justify='center'
@@ -317,6 +405,12 @@ def render_page_content(pathname):
             html.Br(),
             LEAGUE,
             ]
+    elif team_name == 'experimental':
+        return [
+            EXP_LEADERBOARD,
+            html.Br(),
+            EXP_LEAGUE
+        ]
     return make_team_div(team_name)
 
 #####
